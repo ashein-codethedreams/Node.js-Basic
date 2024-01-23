5ec0eedd89ae035953c8391d23aba0ddcb087b98
ashein-codethedreams <heroglasses155@gmail.com>
2024-01-23 10:36:09 +0630

    not found route

--- app.js
+++ app.js
@@ -9,4 +9,9 @@ app.post("/post", () => {
   console.log("you are requesting post route");
 });
 
+//we have to write the following line after all destinated routes
+app.get("*", (req, res) => {
+  res.json({ message: "No Route Found" });
+});
+
 app.listen(3000, console.log("Server is running at port 3000 ..."));
