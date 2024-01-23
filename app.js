b7c1bc18ac5eaba7a4a1d0167de542be7ecf9f17
ashein-codethedreams <heroglasses155@gmail.com>
2024-01-23 10:15:40 +0630

    started server-client communication

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000,()=>console.log("Server is running at port 3000 ..."));
+app.get("/", () => {
+  console.log("you will get testing ..");
+});
+
+app.post("/post", () => {
+  console.log("you are requesting post route");
+});
+
+app.listen(3000, console.log("Server is running at port 3000 ..."));
