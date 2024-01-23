8329179e74465e4b6ecf2a0d62ad7cdbf43f8675
ashein-codethedreams <heroglasses155@gmail.com>
2024-01-23 11:23:33 +0630

    patch and delete method

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res, next) => {
-  res.status(200).json({ name: "Ashein", age: 25, subject: "Node js" });
+app.get("/users", (req, res, next) => {
+  res.status(200).json([
+    { name: "Ashein", age: 25, subject: "Node js" },
+    { name: "Oak Oak", age: 25, subject: "Java" },
+  ]);
 });
 
-app.post("/user", (req, res, next) => {
+app.post("/users", (req, res, next) => {
   res.status(200).json({ message: "user registeration succeeded" });
+});
+
+app.patch("/users/:id/:name", (req, res, next) => {
+  let id = req.params.id;
+  let name = req.params.name;
+  res.status(200).json({ id, name });
+});
+app.delete("/users/:id", (req, res, next) => {
+  let id = req.params.id;
+  res.status(200).json({ message: `Delete user id is ${id}` });
 });
 
 //we have to write the following line after all destinated routes
