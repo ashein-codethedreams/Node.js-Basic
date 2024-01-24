52cbd100ae6320108ef92d5a5d599c458ad3e5c2
ashein-codethedreams <heroglasses155@gmail.com>
2024-01-24 14:02:48 +0630

    route usage

--- routes/posts.js
+++ routes/posts.js
@@ -4,4 +4,14 @@ router.get("/", (req, res) => {
   res.json({ message: "All posts" });
 });
 
+router
+  .route("/:id")
+  .get((req, res) => res.json({ message: "get post id is " + req.params.id }))
+  .patch((req, res) =>
+    res.json({ message: "edit post id is " + req.params.id })
+  )
+  .delete((req, res) =>
+    res.json({ message: "delete post id is " + req.params.id })
+  );
+
 module.exports = router;
